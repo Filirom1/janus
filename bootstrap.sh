821a2d4ce06fc32e240bc39042cd159bb03d942b
Romain <filirom1@gmail.com>
2011-10-17 20:22:04 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-git clone git://github.com/Filirom1/janus.git ~/.vim
+git clone https://github.com/Filirom1/janus.git ~/.vim
 cd ~/.vim
 rake
